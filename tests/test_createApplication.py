0714f72dc03a92aab616e68d10290d1019d951e3
NTyutyunnik <natalka2411>
2018-04-16 18:03:30 +0300

    --success

--- tests/test_createApplication.py
+++ tests/test_createApplication.py
@@ -1,13 +1,14 @@
 from models.soap import SoapMethods
 from datetime import datetime
+from zeep import xsd
 obj = SoapMethods()
-test_set = obj.openConfig()["testSet"]["getCards"] # набор данных для теста с test_data.json
-param = obj.openTestData()[test_set]
+#test_set = obj.openConfig()["testSet"]["getCards"] # набор данных для теста с test_data.json
+#param = obj.openTestData()[test_set]
 wsdl = obj.openConfig()["wsdl"]["createApplication"]
 
 
 def test_createMassApps(obj):
-    f = obj.openXlsFile('D:\Python\SOAP\delivery_1.xlsx')
+    f = obj.openXlsFile('D:\Python\SOAP\delivery_2.xlsx')
     v_count = 0
     for t in f:
         if len(str(f[str(t)][0])) > 1:
@@ -17,6 +18,10 @@ def test_createMassApps(obj):
             objectTypeId = int(str(f[str(t)][12]).split(".")[0])
             dateExp = datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(f[str(t)][16]) - 2).date()
             plannedDeliveryDate=datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(f[str(t)][19]) - 2).date()
+            #listOfNecessaryDocuments = {"documentType": 1, "documentName": "11", "documentDescription": "13"}
+            listOfNecessaryDocuments = [1, 'test', 'test1']
+
+
 
             res = obj.connectToWebService(wsdl).service.createApplication(contragentId=contragentId, taxId=f[str(t)][1],
                                                                           phoneNumber=f[str(t)][2], clientName=str(f[str(t)][3]), deliveryAddress=str(f[str(t)][4]),
@@ -25,7 +30,8 @@ def test_createMassApps(obj):
                                                                           objectTypeId=objectTypeId, comment=str(f[str(t)][13]), #str(f[str(t)][11]),
                                                                           messageIdPrimaryProcess=str(f[str(t)][14]), primaryProcessCode=str(f[str(t)][15]),
                                                                           dateExp=dateExp, externalSystem=str(f[str(t)][17]),
-                                                                          user=str(f[str(t)][18]), plannedDeliveryDate=plannedDeliveryDate)
+                                                                          user=str(f[str(t)][18]), plannedDeliveryDate=plannedDeliveryDate,
+                                                                          listOfNecessaryDocuments=listOfNecessaryDocuments)
 
             v_count += 1
             print(res)
